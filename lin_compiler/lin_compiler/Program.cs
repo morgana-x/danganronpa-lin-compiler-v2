5765518725b45a2bc435133056cb9b07cefe869f
morgana <89588301+morgana-x@users.noreply.github.com>
2024-01-27 01:49:18 +1100

    add translate utility

--- lin_compiler/lin_compiler/Program.cs
+++ lin_compiler/lin_compiler/Program.cs
@@ -38,6 +38,7 @@ namespace LIN
             bool decompile = false;
             string input, output;
             bool dump = false;
+            bool translate = false;
 
             // Parse arguments
             List<string> plainArgs = new List<string>();
@@ -52,6 +53,7 @@ namespace LIN
                     if (a == "-dr2" || a == "--danganronpa2") { game = Game.Danganronpa2; }
                     if (a == "-s" || a == "--silent")         { silentMode = true; }
                     if (a == "-dmp" || a == "--dump") { dump = true; }
+                    if (a == "-trs" || a == "--translate") { translate = true; }
                 }
                 else
                 {
@@ -76,6 +78,12 @@ namespace LIN
             {
                 Console.WriteLine("Dumping selected");
                 Dumper.DumpDirectory(input, output, game);
+                return;
+            }
+            if (translate)
+            {
+                Console.WriteLine("Translating selected");
+                GoogleTranslate.TranslateDirectory(input, output, game);
                 return;
             }
 
